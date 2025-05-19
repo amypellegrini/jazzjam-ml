113625244f409c99bb52fe1338ea35b308da955a
Amy Pellegrini <amyvpellegrini@gmail.com>
2025-05-19 13:08:43 +0200

    feat(fetchData): fetch and return harmonies

--- ml-training/src/fetchData.py
+++ ml-training/src/fetchData.py
@@ -4,15 +4,14 @@ import requests
 def fetchData():
     base_url = "http://localhost:3000"
     sequences_url = base_url + "/walking-bass-sequences"
-    # harmonies_url = base_url + "/walking-bass-harmonies"
+    harmonies_url = base_url + "/walking-bass-harmonies"
 
     try:
         sequences_data = requests.get(sequences_url)
+        harmonies_data = requests.get(harmonies_url)
 
-        # harmonies_data = requests.get(harmonies_url)
-
-        if sequences_data.status_code == 200:
-            return sequences_data.json()
+        if sequences_data.status_code == 200 and harmonies_data.status_code == 200:
+            return sequences_data.json(), harmonies_data.json()
         else:
             return f"Error: Sequences status {sequences_data.status_code}"
 
--- ml-training/src/fetchData_test.py
+++ ml-training/src/fetchData_test.py
@@ -32,5 +32,34 @@ def test_it_fetches_and_returns_data(snapshot):
         ],
     )
 
-    data = str(fetchData())
-    snapshot.assert_match(data, "data.txt")
+    responses.add(
+        responses.GET,
+        "http://localhost:3000/walking-bass-harmonies",
+        json=[
+            {
+                "_id": 0,
+                "keyFifths": 0,
+                "beats": 4,
+                "beatsType": 4,
+                "harmonyRootStep": "D",
+                "divisions": 1,
+                "harmonyRootAlter": 0,
+                "harmonyKind": "minor-seventh",
+                "harmonyDuration": 2,
+                "sequenceId": "95f250d2-e85c-485e-b666-df55fa845257",
+                "startPitchStep": "D",
+                "startPitchOctave": 3,
+                "startPitchAlter": 0,
+                "targetPitchStep": "G",
+                "targetPitchOctave": 3,
+                "targetPitchAlter": 0,
+                "bassStep": None,
+                "bassAlter": 0,
+                "style": "swing",
+            },
+        ],
+    )
+
+    sequences, harmonies = fetchData()
+    snapshot.assert_match(str(sequences), "sequences.txt")
+    snapshot.assert_match(str(harmonies), "harmonies.txt")
